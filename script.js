d26925eb1fb09cba84951c1f2916042de3d7e8e9
Krunal Solanki <98727328+krunal1Solanki@users.noreply.github.com>
2023-03-20 15:57:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+setInterval(() => {
+  let a = fun(document.getElementsByTagName("input")[0]);
+  document.getElementsByTagName("div")[0].innerHTML = a;
+}, 10);
+
+fun = (e) => e.value.length;
